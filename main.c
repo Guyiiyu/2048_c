4f410e73c5268ba9a791f0294b9b20e244a429f9
1v7w <gascd11@163.com>
2023-01-31 23:28:26 +0800

    fix bug and add colorful text.

--- main.c
+++ main.c
@@ -9,20 +9,32 @@
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
 
-#define max(x,y) ((x)>(y)?(x):(y))
-
 int board[BOARDSIZE][BOARDSIZE];
 int score;
-int need_random;
+int need_random = 1;
 int failed;
-int merged;
+
+void print_board_value(int x){
+    if(x == 2) printf("%5d |", x);
+    else if(x == 4) printf("\033[0;32m%5d\033[0m |", x);
+    else if(x == 8) printf("\033[0;33m%5d\033[0m |", x);
+    else if(x == 16) printf("\033[0;91m%5d\033[0m |", x);
+    else if(x == 32) printf("\033[0;96m%5d\033[0m |", x);
+    else if(x == 64) printf("\033[0;93m%5d\033[0m |", x);
+    else if(x == 128) printf("\033[0;94m%5d\033[0m |", x);
+    else if(x == 256) printf("\033[0;95m%5d\033[0m |", x);
+    else if(x == 512) printf("\033[0;96m%5d\033[0m |", x);
+    else if(x == 1024) printf("\033[0;97m%5d\033[0m |", x);
+    else if(x == 2048) printf("\033[0;36m%5d\033[0m |", x);
+    else printf("\033[0;100m%5d\033[0m |", x);
+}
 
 // print the board
 void print_board(){
     system("cls");
-    printf("Score: %d\n", score);
+    printf("Score: \033[0;31m%d\033[0m\n", score);
     if(!failed) printf("\"WASD\" or arrow key to control.");
-    else printf("game over! do you want to replay?[y/n]");
+    else printf("\033[0;31mgame over! do you want to replay?[y/n]\033[0m");
     puts("");puts("");
 
     // board
@@ -32,7 +44,7 @@ void print_board(){
     for(int i=0; i<BOARDSIZE; i++){
         printf("|");
         for(int j=0; j<BOARDSIZE; j++){
-            if(board[i][j]) printf("%5d |", board[i][j]);
+            if(board[i][j]) print_board_value(board[i][j]);
             else printf("      |");
         }
         puts("");
@@ -52,10 +64,10 @@ int random_2(){
         }
     }
     if(len == 0) return -1;
-
+    if(need_random == 0) return 0;
+    need_random = 0;
     int place = blank_list[rand()%len];
-    int vals[2] = {2, 4};
-    board[place/BOARDSIZE][place%BOARDSIZE] = vals[rand()%(merged+1)];
+    board[place/BOARDSIZE][place%BOARDSIZE] = 2;
     return 0;
 }
 
@@ -116,43 +128,36 @@ int get_operation(){
     return res;
 }
 
-//int get_bits(int x){
-//    int res = 0;
-//    while (x){
-//        res++;
-//        x>>=1;
-//    }
-//    return res;
-//}
-
 void update_board(){
     int key = get_operation();
     if(key>=1 && key<=4){
-        need_random = 1;
+        int updated = 0;
         if(key == 1){   // up
             // merge
-            for(int i=0; i<BOARDSIZE; i++){
+            for(int i=0; i<BOARDSIZE; i++) {
                 // last is index
-                int last=-1;
-                for(int j=0; j<BOARDSIZE; j++){
-                    if(board[j][i] == 0) continue;
-                    if(last != -1 && board[last][i] == board[j][i]){
+                int last = -1;
+                for (int j = 0; j < BOARDSIZE; j++) {
+                    if (board[j][i] == 0) continue;
+                    if (last != -1 && board[last][i] == board[j][i]) {
                         board[last][i] *= 2;
-                        merged = 1;
                         score += board[last][i];
                         board[j][i] = 0;
                         last = -1;
+                        updated = 1;
                         continue;
                     }
                     last = j;
                 }
                 // move
-                for(int j=1; j<BOARDSIZE; j++){
+                for (int j = 1; j < BOARDSIZE; j++) {
+                    if (board[j][i] == 0) continue;
                     int k = j;
-                    while(k>=1 && board[k][i] && board[k-1][i]==0) {
+                    while (k >= 1 && board[k][i] && board[k - 1][i] == 0) {
                         board[k - 1][i] = board[k][i];
                         board[k][i] = 0;
                         k--;
+                        updated = 1;
                     }
                 }
             }
@@ -166,21 +171,23 @@ void update_board(){
                     if(board[j][i] == 0) continue;
                     if(last != -1 && board[last][i] == board[j][i]){
                         board[last][i] *= 2;
-                        merged = 1;
                         score += board[last][i];
                         board[j][i] = 0;
                         last = -1;
+                        updated = 1;
                         continue;
                     }
                     last = j;
                 }
                 // move
                 for(int j=BOARDSIZE-2; j>=0; j--){
+                    if(board[j][i] == 0) continue;
                     int k = j;
                     while(k<=BOARDSIZE-2 && board[k][i] && board[k+1][i]==0) {
                         board[k + 1][i] = board[k][i];
                         board[k][i] = 0;
                         k++;
+                        updated = 1;
                     }
                 }
             }
@@ -194,21 +201,23 @@ void update_board(){
                     if(board[i][j] == 0) continue;
                     if(last != -1 && board[i][last] == board[i][j]){
                         board[i][last] *= 2;
-                        merged = 1;
                         score += board[i][last];
                         board[i][j] = 0;
                         last = -1;
+                        updated = 1;
                         continue;
                     }
                     last = j;
                 }
                 // move
                 for(int j=BOARDSIZE-2; j>=0; j--){
+                    if(board[i][j] == 0) continue;
                     int k = j;
                     while(k<=BOARDSIZE-2 && board[i][k] && board[i][k+1]==0) {
                         board[i][k + 1] = board[i][k];
                         board[i][k] = 0;
                         k++;
+                        updated = 1;
                     }
                 }
             }
@@ -222,38 +231,43 @@ void update_board(){
                     if(board[i][j] == 0) continue;
                     if(last != -1 && board[i][last] == board[i][j]){
                         board[i][last] *= 2;
-                        merged = 1;
                         score += board[i][last];
                         board[i][j] = 0;
                         last = -1;
+                        updated = 1;
                         continue;
                     }
                     last = j;
                 }
                 // move
                 for(int j=1; j<BOARDSIZE; j++){
+                    if(board[i][j] == 0) continue;
                     int k = j;
                     while(k>=1 && board[i][k] && board[i][k-1]==0) {
                         board[i][k - 1] = board[i][k];
                         board[i][k] = 0;
                         k--;
+                        updated = 1;
                     }
                 }
             }
         }
+        if(updated) need_random = 1;
     }
     else if(key>=5){
-        if(key == 5){
-            score = failed = merged = 0;
-            need_random = 1;
-            for(int i=0; i<BOARDSIZE; i++)
-                for(int j=0; j<BOARDSIZE; j++)
-                    board[i][j] = 0;
-        }
-        else{
-            printf("Have a good day.\n");
-            system("pause");
-            exit(0);
+        if(failed) {
+            if (key == 5) {
+                score = failed = 0;
+                need_random = 1;
+                for (int i = 0; i < BOARDSIZE; i++)
+                    for (int j = 0; j < BOARDSIZE; j++)
+                        board[i][j] = 0;
+            }
+            else {
+                printf("Have a good day.\n");
+                system("pause");
+                exit(0);
+            }
         }
     }
 }
@@ -265,12 +279,11 @@ int main() {
     print_board();
     while(1){
         update_board();
-        if(need_random) {
-            need_random = 0;
-            if (random_2() == -1){
-                failed = 1;
-            }
+
+        if (random_2() == -1){
+            failed = 1;
         }
+
         print_board();
     }
     return 0;
